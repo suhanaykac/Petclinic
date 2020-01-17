006292b887eec144e9603e5b88b8fe4bbe791d9d
Sühan Aykaç <suhan.aykac@pia-team.com>
2020-01-17 17:44:33 +0300

    petclinic ilk commit

---
+++ src/main/java/com/petclinic/model/Owner.java
@@ -0,0 +1,8 @@
+package com.petclinic.model;
+
+public class Owner {
+	private Long id;
+	private String firstName;
+	private String lastName;
+
+}
---
+++ src/main/java/com/petclinic/model/Pet.java
@@ -0,0 +1,49 @@
+package com.petclinic.model;
+
+import java.util.Date;
+
+public class Pet {
+	private Long id;
+	private String name;
+	private Date birthDate;
+	
+	@Override
+	public String toString() {
+		return "Pet [id=" + id + ", name=" + name + ", birthDate=" + birthDate + ", owner=" + owner + "]";
+	}
+
+	private Owner owner;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Owner getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
+
+}
